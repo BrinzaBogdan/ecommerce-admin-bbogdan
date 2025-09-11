796ab8a1062bd3d2b6febcc34d95361c58048274
BrinzaBogdan <bogdan.brinza2020@gmail.com>
2025-09-11 14:49:09 +0300

    added the actions copyid, update, delete for all the billboard types of things to happen

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,37 @@
-import { BillboardClient } from "./components/client";
+import { format } from "date-fns";
 
-const BillboardsPage = () => {
+
+import prismadb from "@/lib/prismadb";
+import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
+
+const BillboardsPage = async ({
+  params,
+}: {
+  params: Promise<{ storeId: string }>;
+}) => {
+  const { storeId } = await params; 
+
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMM do, yyyy")
+  }))
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={billboards} />
       </div>
     </div>
   );
